aadfb9270ab22fdc211131ab2079010b398ab975
Bogdan Popa <bogdan@defn.io>
2020-02-16 11:57:17 +0200

    clean up code examples and add info on deta

--- talk.rkt
+++ talk.rkt
@@ -64,7 +64,7 @@
 (demo)
 
 (let ([it (lambda (s)
-            (para (small (t s))))])
+            (para (t s)))])
   (slide
    #:title "What's missing?"
    (it "form validation*")
@@ -82,77 +82,107 @@
 
 ;; Form validation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (roman n)
+  (cond
+    [(< n 4) (make-string n #\i)]
+    [(= n 4) "iv"]
+    [(< n 9) (~a "v" (make-string (- n 5) #\i))]
+    [(= n 9) "ix"]
+    [(> n 9) (~a "x" (roman (- n 10)))]))
+
+(define (pill p)
+  (define bg (colorize
+              (filled-rounded-rectangle (+ (pict-width p) 20)
+                                        (+ (pict-height p) 10)
+                                        10)
+              "white"))
+  (cc-superimpose bg p))
+
+(define (make-stepper)
+  (define count 0)
+  (lambda (s)
+    (set! count (add1 count))
+    (define txt (small (red (t (format "~a. ~a" (roman count) s)))))
+    (define wrap (colorize
+                  (filled-rectangle (pict-width txt)
+                                    (+ (pict-height txt) 20)
+                                    #:border-width #f)
+                  "white"))
+    (pill (lt-superimpose wrap txt))))
+
 (slide
  #:title "Form validation"
- (small (para (lib formlets) (t "come with") (lib web-server-lib) (t "but...")))
- (item (small (t "they couple presentation to validation")))
- (item (small (t "no easy way to include validation errors into the output")) ))
+ (para (lib formlets) (t "come with") (lib web-server-lib) (t "but..."))
+ (item (t "they couple presentation to validation"))
+ (item (t "no easy way to include validation errors into the output")))
 
-(let* ([step-c 0]
-       [step (lambda (s)
-               (set! step-c (add1 step-c))
-               (small (red (t (format "~a. ~a" step-c s)))))])
+(let ([step (make-stepper)])
   (slide
    #:title (para (titlet "Form validation:") (lib forms-lib) #:align 'center)
    #:name "forms-lib"
    (ht-append
     20
     (vl-append
-     10
+     -20
      (step "Declare a form...")
-     (small
-      (small (code
-              (define login-form
-                (form* ([username (ensure binding/email (required))]
-                        [password (ensure binding/text (required))])
-                  (list username password)))))))
+     (pill
+      (small
+       (small (code
+               (define login-form
+                 (form* ([username (ensure binding/email (required))]
+                         [password (ensure binding/text (required))])
+                   (list username password))))))))
     (vl-append
-     10
+     -20
      (step "Declare a renderer for it...")
+     (pill
+      (small
+       (small (code
+               (define (render-login-form rw)
+                 `(form
+                   (label "Username" ,(rw "username" (widget-email)))
+                   ,@(rw "username" (widget-errors))
+                   (label "Passowrd" ,(rw "password" (widget-password)))
+                   ,@(rw "password" (widget-errors))
+                   (button ([type "submit"]) "Login")))))))))
+   (vl-append
+    -20
+    (step "Validate requests against it...")
+    (pill
      (small
       (small (code
-              (define (render-login-form rw)
-                `(form
-                  (label "Username" ,(rw "username" (widget-email)))
-                  ,@(rw "username" (widget-errors))
-                  (label "Passowrd" ,(rw "password" (widget-password)))
-                  ,@(rw "password" (widget-errors))
-                  (button ([type "submit"]) "Login"))))))))
-   (vl-append
-    10
-    (step "Validate requests against it...")
-    (small
-     (small (code
-             (define (login req)
-               (match (form-run login-form req)
-                 [(list 'pending _ rw)
-                  (response/xexpr
-                   (render-login-form rw))]
-                 (code:line)
-                 [(list 'failed errors rw)
-                  (response/xexpr
-                   (render-login-form rw))]
-                 (code:line)
-                 [(list 'passed (list username password) rw)
-                  (login! username password)
-                  (redirect-to "/dashboard")]))))))))
+              (define (login req)
+                (match (form-run login-form req)
+                  [(list 'pending _ rw)
+                   (response/xexpr
+                    (render-login-form rw))]
+                  (code:line)
+                  [(list 'failed errors rw)
+                   (response/xexpr
+                    (render-login-form rw))]
+                  (code:line)
+                  [(list 'passed (list username password) rw)
+                   (login! username password)
+                   (redirect-to "/dashboard")])))))))))
 
 (demo)
 
 
 ;; Database migrations ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(slide
- #:title (para (titlet "Database migrations:") (lib north) #:align 'center)
- #:name "north"
- (ht-append
-  20
-  (vl-append
-   10
-   (small (red (t "A language for declaring migrations:")))
-   (small
-    (small
-     (codeblock-pict #<<CODE
+(let ([step (make-stepper)])
+  (slide
+   #:title (para (titlet "Database migrations:") (lib north) #:align 'center)
+   #:name "north"
+   (ht-append
+    -50
+    (vl-append
+     -20
+     (step "A #lang for declaring migrations...")
+     (pill
+      (small
+       (small
+        (codeblock-pict #<<CODE
 #lang north
 
 -- @revision: 2b39dc065de3cd5ebb612e2d80f00d8c
@@ -177,27 +207,104 @@ create table users(
 drop table users;
 -- }
 CODE
-                     ))))
-  (vl-append
-   10
-   (small (red (t "A CLI tool for managing them:")))
-   (small
+                        )))))
     (vl-append
-     10
-     (code |raco north create|)
-     (code |raco north migrate|)
-     (code |raco north rollback|)
-     (code |raco north show|))))))
+     -20
+     (step "A CLI tool for managing them...")
+     (pill
+      (small
+       (codeblock-pict #<<CODE
+#lang rash
+raco north create
+raco north migrate
+raco north rollback
+raco north show
+CODE
+                       #:keep-lang-line? #f
+                       )))))))
 
 (demo)
 
 
 ;; ORM ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(let ([step (make-stepper)])
+  (slide
+   #:title (para (titlet "ORM:") (lib deta-lib) #:align 'center)
+   #:name "deta"
+   (t "An ORM without the \"R\"elational part.")
+   (vl-append
+    20
+    (vl-append
+     -20
+     (step "Maps table rows to structs...")
+     (pill
+      (vl-append
+       10
+       (small
+        (code
+         (define-schema user
+           #:table "users"
+           ([id id/f #:primary-key #:auto-increment]
+            [username string/f]
+            [(password-hash #f) string/f]))))
+
+       (small
+        (code
+         (define u
+           (make-user #:username "Bogdan"))))
+
+       (small
+        (code
+         (insert! conn u))))))
+    (vl-append
+     -20
+     (step "Constructs SQL queries using Racket DSL...")
+     (pill
+      (small
+       (code
+        (lookup conn (~> (from user #:as u)
+                         (where (= u.username "Bogdan")))))))))))
+
 (slide
- #:title (para (titlet "ORM:") (lib deta-lib) #:align 'center)
- #:name "deta"
- (subscript (t "* not really an ORM")))
+ #:title (para (titlet "ORM:") (lib deta-lib) (t "cont'd.") #:align 'center)
+ #:name "deta 2"
+ (t "Can produce structs for arbitrary result sets. Great for analytical queries:")
+ (ht-append
+  10
+  (pill
+   (small
+    (small
+     (code
+      (define-schema orders/status/month
+        #:virtual
+        ([status symbol/f]
+         [month datetime-tz/f]
+         [orders integer/f]
+         [revenue integer/f]))))))
+
+  (pill
+   (small
+    (small
+     (code
+      (~> (from "orders" #:as o)
+          (join "order_statuses" #:as os #:on (= o.id os.order-id))
+          (select (as os.status status)
+                  (as (date-trunc "month" o.created-at) month)
+                  (as (count *) orders)
+                  (as 0 revenue))
+          (group-by 1 2)
+          (union (~> (from "orders" #:as o)
+                     (join "order_totals" #:as ot #:on (= o.id ot.order-id))
+                     (join "order_statuses" #:as os #:on (= o.id os.order-id))
+                     (select (as "revenue" status)
+                             (as (date-trunc "month" o.created-at) month)
+                             (as 0 orders)
+                             (as (sum ot.total) revenue))
+                     (where (not (in os.status (list "canceled" "returned"))))
+                     (group-by 1 2)))
+          (order-by ([month #:asc]))
+          (project-onto orders/status/month-schema))))))))
 
 (demo)
 
@@ -205,8 +312,17 @@ CODE
 ;; E2E Testing ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (slide
- #:title (para (titlet "E2E testing:") (lib marionette) #:align 'center)
- #:name "marionette")
+ #:title (para (titlet "E2E testing:") (lib marionette-lib) #:align 'center)
+ #:name "marionette"
+ (t "A library that lets you drive a Firefox process from Racket:")
+ (pill
+  (code
+   (call-with-marionette/browser/page!
+    (lambda (p)
+      (page-goto! p "https://github.com")
+      (call-with-page-screenshot! p
+        (lambda (data)
+          ...)))))))
 
 (demo)
 
@@ -222,7 +338,31 @@ CODE
 
 ;; Fin ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(with-font "Georgia"
-  (slide
-   (caps (big (t "Thanks!")))
-   (red (t "defn.io"))))
+(define (fin)
+  (with-font "Georgia"
+    (slide
+     (caps (big (t "Thanks!")))
+     (red (t "defn.io")))))
+
+(fin)
+
+
+;; ITT: Geolocation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(slide
+ #:title (para (titlet "Geolocation:") (lib geoip-lib) #:align 'center)
+ #:name "geoip"
+ )
+
+
+;; ITT: Sentry ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(slide
+ #:title (para (titlet "Error monitoring:") (lib sentry-lib) #:align 'center)
+ #:name "sentry"
+ )
+
+
+;; Fin (for real!) ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fin)
